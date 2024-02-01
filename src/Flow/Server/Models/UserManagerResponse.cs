27897beb5eebd21947629e4a74cdaa70cfb6d481
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-02-01 20:01:23 +0300

    Added authentication in Program.cs

---
+++ src/Flow/Server/Models/UserManagerResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Flow.Server.Models;
+
+public class UserManagerResponse
+{
+    public required string Message { get; set; }
+    public string? Token { get; set; }
+    public bool Succeeded { get; set; }
+    public bool IsLockedOut { get; set; }
+}
---
+++ src/Flow/Server/Options/JwtOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Flow.Server.Options;
+
+public class JwtOptions
+{
+    public string Secret { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public bool ValidateLifetime { get; set; }
+    public bool ValidateIssuer { get; set; }
+    public bool ValidateAudience { get; set; }
+    public bool RequiresExpirationTime { get; set; }
+    public int ExpiresIn { get; set; }
+}
