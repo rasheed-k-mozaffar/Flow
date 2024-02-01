d34d9657616cde346166c47a9bf71e0d1bce8c58
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-02-01 20:00:29 +0300

    Created types for the different api responses

---
+++ src/Flow/Shared/ApiResponses/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace Flow.Shared.ApiResponses;
+
+public class ApiResponse
+{
+    public required string Message { get; set; }
+    public bool IsSuccess { get; set; }
+}
+
+public class ApiResponse<T> : ApiResponse
+{
+    public T? Body { get; set; }
+}
