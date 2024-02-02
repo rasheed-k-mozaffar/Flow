7a68fbcc0db2d967d47cd5a34e26319d2d517d60
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-02-02 14:30:45 +0300

    Fixed string length issue

--- src/Flow/Shared/AuthRequests/LoginRequest.cs
+++ src/Flow/Shared/AuthRequests/LoginRequest.cs
@@ -9,7 +9,8 @@ public class LoginRequest
     public string? Email { get; set; }
 
     [Required(ErrorMessage = "Your password is required")]
-    [Range(6, 25, ErrorMessage = "The password must be between 6-25 characters")]
+    [MinLength(6, ErrorMessage = "Your password must be at least 6 characters long")]
+    [MaxLength(25, ErrorMessage = "Your password cannot be longer than 25 characters")]
     public string? Password { get; set; }
 
     public bool IsPersistent { get; set; }
