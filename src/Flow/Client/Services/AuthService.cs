649b5d7eb412afe61166e74032a76d68e2c112f5
hamza <hamzabzamreek@gmail.com>
2024-02-03 01:28:05 -0800

    Auto stash before merge of "SignupAnimations" and "origin/SignupAnimations"

---
+++ src/Flow/Client/Services/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using Flow.Shared.ApiResponses;
+using System.Net.Http.Json;
+
+namespace Flow.Client.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ApiResponse<string>> RegisterUserAsync(RegisterRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync("/api/auth/register", request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                throw new AuthFailedException(message: error!.ErrorMessage!);
+            }
+
+            var result = await response.Content.ReadFromJsonAsync<ApiResponse<string>>();
+            return result!;
+        }
+        public async Task<ApiResponse<string>> LoginUserAsync(LoginRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync("/api/auth/login", request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                throw new AuthFailedException(message: error!.ErrorMessage!);
+            }
+
+            var result = await response.Content.ReadFromJsonAsync<ApiResponse<string>>();
+            return result!;
+        }
+    }
+}
