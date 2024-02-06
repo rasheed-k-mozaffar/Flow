cea3751525f2f8220e246a9584233d05f61471da
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-06 23:28:49 +0300

    Created a contact dto to use in GetContacts endpoint

---
+++ src/Flow/Shared/DataTransferObjects/ContactDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Flow.Shared.DataTransferObjects;
+
+public class ContactDto
+{
+    public Guid RequestId { get; set; }
+    public Guid? ThreadId { get; set; }
+    public UserDetailsDto? Contact { get; set; }
+}
