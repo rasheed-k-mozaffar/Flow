a3f5813bd9cd218da7ec0ad0743323999e97da07
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-07 14:54:54 +0300

    Created and implemented the IContactsRepo

---
+++ src/Flow/Server/Repositories/ContactRequestsRepository.cs
@@ -0,0 +1,204 @@
+﻿using Flow.Shared.Enums;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Flow.Server.Repositories;
+
+public class ContactRequestsRepository : IContactRequestsRepository
+{
+    private readonly AppDbContext _db;
+    private readonly ILogger<ContactRequestsRepository> _logger;
+    private readonly UserInfo _userInfo;
+
+    public ContactRequestsRepository
+    (
+        AppDbContext db,
+        ILogger<ContactRequestsRepository> logger,
+        UserInfo userInfo
+    )
+    {
+        _db = db;
+        _logger = logger;
+        _userInfo = userInfo;
+    }
+
+
+    public async Task SendConnectRequestAsync(string userId, ContactRequest request)
+    {
+        // find the recipient
+        var recipient = await _db
+                            .Users
+                            .FirstOrDefaultAsync(u => u.Id == request.RecipientId);
+
+        if (recipient is null)
+            throw new UserNotFoundException(message: "User not found");
+
+        request.SenderId = _userInfo.UserId!;
+
+        // process the connect request
+        var entityEntry = await _db
+                                .ContactRequests
+                                .AddAsync(request);
+
+        if (entityEntry.State is EntityState.Added)
+        {
+            // TODO: Refactor the notification part into its own repo (INotifcations)
+            var notification = new Notification()
+            {
+                Id = Guid.NewGuid(),
+                RecipientId = request.RecipientId,
+                Recipient = recipient,
+                Content = $"{_userInfo.Name} wants to connect with you",
+                IssuedOn = DateTime.UtcNow,
+                Type = NotificationType.ContactRequest
+            };
+
+            await _db.Notifications.AddAsync(notification); // push notification to the db
+
+            await _db.SaveChangesAsync();
+
+            _logger.LogInformation
+            (
+                "{username} sent contact request to {recipient}",
+                _userInfo.Name,
+                recipient.UserName
+            );
+
+            return;
+        }
+
+        _logger.LogError
+        (
+            "Connect request between {username} and {recipient} failed",
+            _userInfo.Name,
+            recipient.UserName
+        );
+
+        throw new ContactRequestOperationFailureException(message: "Something went wrong while sending request");
+    }
+
+
+    public async Task CancelConnectRequestAsync(Guid requestId)
+    {
+        var request = await _db
+                            .ContactRequests
+                            .FindAsync(requestId);
+
+        if (request is null)
+            throw new ResourceNotFoundException(message: "The request you're looking for was not found");
+
+        var deletionResult = _db.ContactRequests.Remove(request);
+
+        if (deletionResult.State is EntityState.Deleted)
+        {
+            _logger.LogInformation
+            (
+                "{senderId} cancelled contact request with {recipientId}",
+                request.SenderId,
+                request.RecipientId
+            );
+
+            await _db.SaveChangesAsync();
+            return;
+        }
+
+        throw new ContactRequestOperationFailureException(message: "Something went wrong while cancelling the request");
+    }
+
+
+    public async Task ResolveConnectRequestAsync(Guid requestId, RequestStatus newStatus)
+    {
+        var request = await _db
+                            .ContactRequests
+                            .Include(p => p.Sender)
+                            .Include(p => p.Recipient)
+                            .FirstAsync(req => req.Id == requestId);
+
+        if (request is null)
+            throw new ResourceNotFoundException(message: "The request you're looking for was not found");
+
+        if (request.Status is RequestStatus.Declined || request.Status is RequestStatus.Accepted)
+            throw new InvalidContactOperationException(message: "Request already responded to");
+
+        if (newStatus is RequestStatus.Accepted)
+        {
+            // new up a thread for the users involved in the contact request
+            var thread = new ChatThread()
+            {
+                Id = Guid.NewGuid(),
+                Participants = new List<AppUser> { request.Sender, request.Recipient },
+                CreatedAt = DateTime.UtcNow
+            };
+
+            request.Status = RequestStatus.Accepted;
+            request.ChatThreadId = thread.Id;
+            request.ChatThread = thread;
+
+            await _db.Threads.AddAsync(thread);
+
+            _logger.LogInformation
+            (
+                "{recipientName} accepted contact request from {sender} & new chat thread was created: Thread ID: {threadId}",
+                request.Recipient.UserName,
+                request.Sender.UserName,
+                thread.Id
+            );
+
+            await _db.SaveChangesAsync();
+            return;
+        }
+        else
+        {
+            // in case the request was declined, remove it from the db
+            var deletionResult = _db.ContactRequests.Remove(request);
+
+            if (deletionResult.State is EntityState.Deleted)
+            {
+                _logger.LogInformation
+                (
+                    "{recipientName} declined contact request with {sender}",
+                    request.Recipient.UserName,
+                    request.Sender.UserName
+                );
+
+                await _db.SaveChangesAsync();
+                return;
+            }
+        }
+    }
+
+
+    public async Task<IEnumerable<ContactRequest>> GetPendingContactRequestsAsync(UserType userType)
+    {
+        var pendingRequests = userType switch
+        {
+            // * Requests Sent
+            UserType.Sender => await _db
+                                    .ContactRequests
+                                    .Where(req => req.SenderId == _userInfo.UserId)
+                                    .Where(req => req.Status == RequestStatus.Pending)
+                                    .ToListAsync(),
+
+            // * Requests Received
+            UserType.Recipient => await _db
+                                    .ContactRequests
+                                    .Where(req => req.RecipientId == _userInfo.UserId)
+                                    .Where(req => req.Status == RequestStatus.Pending)
+                                    .ToListAsync(),
+
+            _ => throw new ArgumentException("Invalid user type")
+        };
+
+        return pendingRequests;
+    }
+
+
+    public async Task<IEnumerable<ContactRequest>> GetUserContactsAsync()
+    {
+        var contacts = await _db
+                            .ContactRequests
+                            .Where(cr => (cr.SenderId == _userInfo.UserId || cr.RecipientId == _userInfo.UserId) && cr.Status == RequestStatus.Accepted)
+                            .ToListAsync();
+
+        return contacts;
+    }
+}
---
+++ src/Flow/Server/Repositories/IContactRequestsRepository.cs
@@ -0,0 +1,46 @@
+﻿using Flow.Shared.Enums;
+
+namespace Flow.Server.Repositories;
+
+public interface IContactRequestsRepository
+{
+    /// <summary>
+    /// Adds a new contact request to the database, and a notification under the recipients id 
+    /// to notify them about a pending contact request
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    Task SendConnectRequestAsync(string username, ContactRequest request);
+
+    /// <summary>
+    /// In case the request got accepted or declined, resolve the new status in the database
+    /// If the new status is Accepted, a new chat thread is added to the database between the sender and recipient of
+    /// the contact request.
+    /// </summary>
+    /// <returns></returns>
+    Task ResolveConnectRequestAsync(Guid requestId, RequestStatus newStatus);
+
+    /// <summary>
+    /// Cancels a contact request from the sender's end, this would delete the record for it in the database
+    /// </summary>
+    /// <param name="requestId"></param>
+    /// <returns></returns>
+    Task CancelConnectRequestAsync(Guid requestId);
+
+    /// <summary>
+    /// Gets all the pending contact requests for the correspnding user type
+    /// </summary>
+    /// <param name="userType">
+    ///     UserType.Sender: Gets the contact requests the user has made to other users (Pending)
+    ///     UserType.Recipient: Gets the contact requests the user has received from other users
+    /// </param>
+    /// <returns></returns>
+    Task<IEnumerable<ContactRequest>> GetPendingContactRequestsAsync(UserType userType);
+
+    /// <summary>
+    /// Gets the contacts for the user based on the ContactRequests table
+    /// Any contact request the user has sent, or received, if accepted, it's a contact to be retrieved
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<ContactRequest>> GetUserContactsAsync();
+}
