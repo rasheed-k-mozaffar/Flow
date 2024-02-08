38553b14f9eb63483fe70f8750f19309e552f3ec
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-08 23:02:23 +0300

    Created a dto for image objects

---
+++ src/Flow/Server/Helpers/ImageDataTransferObjectsMapper.cs
@@ -0,0 +1,20 @@
+﻿namespace Flow.Server.Helpers;
+
+public static class ImageDataTransferObjectsMapper
+{
+    /// <summary>
+    /// Maps an image to an image dto used to transfer back image details in the response
+    /// </summary>
+    /// <param name="image"></param>
+    /// <returns></returns>
+    public static ImageDto ToImageDto(this Image image)
+    {
+        return new ImageDto
+        {
+            Id = image.Id,
+            RelativeUrl = image.RelativeUrl,
+            AppUserId = image.AppUserId,
+            Type = image.Type
+        };
+    }
+}
---
+++ src/Flow/Shared/DataTransferObjects/ImageDto.cs
@@ -0,0 +1,11 @@
+﻿using Flow.Shared.Enums;
+
+namespace Flow.Shared.DataTransferObjects;
+
+public class ImageDto
+{
+    public Guid Id { get; set; }
+    public string? RelativeUrl { get; set; }
+    public string? AppUserId { get; set; }
+    public ImageType Type { get; set; }
+}
