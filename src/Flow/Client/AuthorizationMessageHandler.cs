f0e24639a56013ffcd8be6ad4082edfab31346bb
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-14 18:31:00 +0300

    Implemented the authorization message handler

---
+++ src/Flow/Client/AuthorizationMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Flow.Client;
+
+public class AuthorizationMessageHandler : DelegatingHandler
+{
+    private readonly IJwtsManager _jwtsManager;
+
+    public AuthorizationMessageHandler(IJwtsManager jwtsManager)
+    {
+        _jwtsManager = jwtsManager;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (await _jwtsManager.CheckForJwtAsync())
+        {
+            string? tokenString = await _jwtsManager.GetJwtAsync();
+
+            // Set the authorization header to use Bearer and write the token in it
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", tokenString);
+
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
