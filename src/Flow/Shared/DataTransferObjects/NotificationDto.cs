78fba98e4d9e29a0299d1fe92a3717e403383438
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-17 22:58:43 +0300

    Created a notification dto to send notifications to clients

---
+++ src/Flow/Shared/DataTransferObjects/NotificationDto.cs
@@ -0,0 +1,13 @@
+﻿using Flow.Shared.Enums;
+
+namespace Flow.Shared.DataTransferObjects;
+
+public class NotificationDto
+{
+    public Guid Id { get; set; }
+    public NotificationType Type { get; set; }
+    public string? Content { get; set; }
+    public bool Seen { get; set; }
+    public DateTime IssuedOn { get; set; }
+    public string? RecipientId { get; set; }
+}
