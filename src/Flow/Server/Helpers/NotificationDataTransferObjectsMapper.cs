d939b7b5fcd1d0f6b34d5caf053bf4431339d1b8
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-17 23:00:35 +0300

    Created a mapper class for notifications

---
+++ src/Flow/Server/Helpers/NotificationDataTransferObjectsMapper.cs
@@ -0,0 +1,17 @@
+﻿namespace Flow.Server.Helpers;
+
+public static class NotificationDataTransferObjectsMapper
+{
+    public static NotificationDto ToNotificationDto(this Notification notification)
+    {
+        return new()
+        {
+            Id = notification.Id,
+            Content = notification.Content,
+            Type = notification.Type,
+            IssuedOn = notification.IssuedOn,
+            Seen = notification.Seen,
+            RecipientId = notification.RecipientId
+        };
+    }
+}
