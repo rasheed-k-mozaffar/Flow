e1dbc8fe5d7f14e18e15d1111bd55e8bd19c38b5
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-26 20:04:49 +0300

    Created classes to use when sending messages with Signal R

---
+++ src/Flow/Shared/DataTransferObjects/MessageDto.cs
@@ -0,0 +1,13 @@
+﻿using Flow.Shared.Enums;
+
+namespace Flow.Shared.DataTransferObjects;
+
+public class MessageDto
+{
+    public required Guid Id { get; set; }
+    public required Guid ThreadId { get; set; }
+    public required string SenderId { get; set; }
+    public required string Content { get; set; }
+    public DateTime SentOn { get; set; }
+    public MessageStatus Status { get; set; }
+}
---
+++ src/Flow/Shared/DataTransferObjects/SendMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Flow.Shared.DataTransferObjects;
+
+public class SendMessageDto
+{
+    public Guid MessageId { get; set; } = Guid.NewGuid();
+    public Guid ThreadId { get; set; }
+    public string SenderId { get; set; } = null!;
+    public string Content { get; set; } = null!;
+}
