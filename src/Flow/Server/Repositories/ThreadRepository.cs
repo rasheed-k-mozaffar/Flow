df68f9638dfeefa8f3a6baaf4a891d6fd539fae7
Rasheed <rasheedkmozaffar@hotmail.com>
2024-02-27 20:43:27 +0300

    Refactored the repository and controller

--- src/Flow/Server/Repositories/ThreadRepository.cs
+++ src/Flow/Server/Repositories/ThreadRepository.cs
@@ -1,46 +1,39 @@
-﻿namespace Flow.Server.Repositories
+﻿namespace Flow.Server.Repositories;
+
+public class ThreadRepository : IThreadRepository
 {
-    public class ThreadRepository : IThreadRepository
+    private const int MESSAGES_LOAD_SIZE = 20;
+    private readonly AppDbContext _db;
+    private readonly ContactRequestsRepository _contactsRepository;
+
+    public ThreadRepository(AppDbContext db, ContactRequestsRepository contactsRepository)
     {
-        private readonly AppDbContext _DbContext;
-        private readonly ContactRequestsRepository _CrRepo;
-        public ThreadRepository(AppDbContext AppDbContext, ContactRequestsRepository ContactRequestsRepository) {
-            _DbContext = AppDbContext;
-            _CrRepo = ContactRequestsRepository;
-        }
-        public async Task<Dictionary<string, List<Message>>> GetPreliminaryMessages() 
+        _db = db;
+        _contactsRepository = contactsRepository;
+    }
+
+    public async Task<Dictionary<string, List<MessageDto>>> GetPreliminaryMessagesForUserChatThreads()
+    {
+        var messagesOfEachThread = new Dictionary<string, List<MessageDto>>();
+
+        var userContacts = await _contactsRepository.GetUserContactsAsync();
+
+        foreach (var contact in userContacts)
         {
-            var messagesOfEachThread = new Dictionary<string, List<Message>>();
+            string threadId = contact.ChatThreadId.ToString()!;
 
-            var userThreads = await _CrRepo.GetUserContactsAsync();
-            foreach(var thread in userThreads)
-            {
-                string threadId = thread.ChatThreadId.ToString()!;
-                var ThreadMessages = await _DbContext.Messages.Where(message => message.ThreadId == thread.Id)
-                    .OrderByDescending(message => message.SentOn)
-                    .Take(15)
-                    .ToListAsync();
-                messagesOfEachThread.Add(threadId, ThreadMessages);
-            }
+            var threadMessages = await _db
+                                    .Messages
+                                    .Where(message => message.ThreadId == contact.ChatThreadId)
+                                    .OrderByDescending(message => message.SentOn)
+                                    .Take(MESSAGES_LOAD_SIZE)
+                                    .Select(msg => msg.ToMessageDto())
+                                    .ToListAsync();
 
-            return messagesOfEachThread;
+            messagesOfEachThread.Add(threadId, threadMessages);
         }
-        public async Task<Dictionary<string, List<MessageDto>>> GetPreliminaryMessagesDTO()
-        {
-            var messagesOfEachThread = new Dictionary<string, List<MessageDto>>();
-            var userThreads = await _CrRepo.GetUserContactsAsync();
-            foreach (var thread in userThreads)
-            {
-                string threadId = thread.ChatThreadId.ToString()!;
-                var ThreadMessages = await _DbContext.Messages.Where(message => message.ThreadId == thread.Id)
-                    .OrderByDescending(message => message.SentOn)
-                    .Take(15)
-                    .Select(m => m.ToMessageDto())
-                    .ToListAsync();
-                messagesOfEachThread.Add(threadId, ThreadMessages);
-            }
 
-            return messagesOfEachThread;
-        }
+        return messagesOfEachThread;
     }
 }
+
