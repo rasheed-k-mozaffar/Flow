181841707678df1e8dc6eb5ebb8f5edc3125d969
Rasheed <rasheedkmozaffar@hotmail.com>
2024-03-01 19:43:50 +0300

    Created and registered a messages repo and the chat hub

--- src/Flow/Server/Helpers/MessageDataTransferObjectsMapper.cs
+++ src/Flow/Server/Helpers/MessageDataTransferObjectsMapper.cs
@@ -2,16 +2,41 @@
 {
     public static class MessageDataTransferObjectsMapper
     {
+        public static Message ToMessage(this SendMessageDto message)
+        {
+            return new()
+            {
+                SenderId = message.SenderId,
+                ThreadId = message.ThreadId,
+                Content = message.Content,
+                SentOn = DateTime.UtcNow,
+                Status = Shared.Enums.MessageStatus.Delivered,
+            };
+        }
+
         public static MessageDto ToMessageDto(this Message message)
         {
             return new MessageDto
             {
                 Id = message.Id,
-                ThreadId=message.ThreadId,
-                SenderId=message.SenderId,
+                ThreadId = message.ThreadId,
+                SenderId = message.SenderId,
                 Content = message.Content,
                 SentOn = message.SentOn,
                 Status = message.Status,
+            };
+        }
+
+        public static MessageDto ToMessageReceivedDto(this SendMessageDto sentMessage)
+        {
+            return new()
+            {
+                Id = sentMessage.MessageId,
+                ThreadId = sentMessage.ThreadId,
+                Content = sentMessage.Content,
+                SenderId = sentMessage.SenderId,
+                Status = Shared.Enums.MessageStatus.Delivered,
+                SentOn = DateTime.UtcNow
             };
         }
     }
