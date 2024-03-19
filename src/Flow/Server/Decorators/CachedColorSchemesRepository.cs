c0b13b3fa134dc4c9c940a4f3f93757219b3d642
Rasheed <rasheedkmozaffar@hotmail.com>
2024-03-19 21:01:05 +0300

    Implemented two decorators for the new repos

---
+++ src/Flow/Server/Decorators/CachedColorSchemesRepository.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Flow.Server.Decorators;
+
+public class CachedColorSchemesRepository : IColorSchemesRepository
+{
+    private readonly ColorSchemesRepository _decorated;
+    private readonly IMemoryCache _cache;
+
+    public CachedColorSchemesRepository(ColorSchemesRepository schemesRepository, IMemoryCache cache)
+    {
+        _decorated = schemesRepository;
+        _cache = cache;
+    }
+
+    public async Task<ColorScheme> GetSchemeAsync(int schemeId)
+    {
+        try
+        {
+            var scheme = await _decorated.GetSchemeAsync(schemeId);
+            return scheme;
+        }
+        catch (ResourceNotFoundException)
+        {
+            throw;
+        }
+    }
+
+    public async Task<IEnumerable<ColorScheme>> GetSchemesAsync()
+    {
+        string key = "color-schemes";
+
+#pragma warning disable CS8603 // Possible null reference return.
+        return await _cache.GetOrCreateAsync(
+            key,
+            entry =>
+            {
+                entry.SetAbsoluteExpiration(TimeSpan.FromDays(1));
+
+                return _decorated.GetSchemesAsync();
+            });
+#pragma warning restore CS8603 // Possible null reference return.
+    }
+}
