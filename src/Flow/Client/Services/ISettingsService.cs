1258293534717cb0d611fe1c79137a29595ea918
Rasheed <rasheedkmozaffar@hotmail.com>
2024-03-19 21:01:51 +0300

    Implemented the settings client service

---
+++ src/Flow/Client/Services/ISettingsService.cs
@@ -0,0 +1,8 @@
+﻿using Flow.Shared.ApiResponses;
+
+namespace Flow.Client.Services;
+
+public interface ISettingsService
+{
+    Task<ApiResponse<UserSettingsDto>> GetUserSettingsAsync();
+}
---
+++ src/Flow/Client/Services/SettingsService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using Flow.Shared.ApiResponses;
+
+namespace Flow.Client.Services;
+
+public class SettingsService : ISettingsService
+{
+    private const string BASE_URL = "/api/settings";
+    private const string GET_SETTINGS_URL = $"{BASE_URL}/get-settings";
+
+    private readonly HttpClient _httpClient;
+
+    public SettingsService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ApiResponse<UserSettingsDto>> GetUserSettingsAsync()
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(GET_SETTINGS_URL);
+        response.EnsureSuccessStatusCode();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorResponse = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+            throw new ApiGetRequestFailedException(errorResponse!.ErrorMessage);
+        }
+
+        var data = await response.Content.ReadFromJsonAsync<ApiResponse<UserSettingsDto>>();
+        return data!;
+    }
+}
