e699a2eab3b1786fe6ec71d6a806661907848cb7
Rasheed <rasheedkmozaffar@hotmail.com>
2024-03-24 21:51:54 +0300

    Modified the load previous messages method

---
+++ src/Flow/Shared/DataTransferObjects/PreviousMessagesResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Flow.Shared.DataTransferObjects;
+
+public class PreviousMessagesResponse
+{
+    public List<MessageDto> Messages { get; set; }
+    public bool HasUnloadedMessages { get; set; }
+    public int UnloadedMessageCount { get; set; }
+
+    public PreviousMessagesResponse()
+    {
+        Messages = new();
+    }
+}
