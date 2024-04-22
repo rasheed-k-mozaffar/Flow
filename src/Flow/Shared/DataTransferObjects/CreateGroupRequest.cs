8ff192d7d38ae60b16fd80f3e6cc896f21cc0256
Rasheed <rasheedkmozaffar@hotmail.com>
2024-04-22 08:55:49 +0300

    Added DTOs to use in groups related functions

---
+++ src/Flow/Shared/DataTransferObjects/CreateGroupRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Flow.Shared.DataTransferObjects;
+
+public class CreateGroupRequest
+{
+    public string? GroupName { get; set; }
+    public string? Description { get; set; }
+    public ICollection<string> Participants { get; set; } = new List<string>();
+}
